65ae0a3c23ac5f88df312681440ac0e6322f5055
Michel SCHMIT <mschmit@e2r1p13.42.fr>
2014-05-26 17:02:49 +0200

    pwd ok

--- src/builtin/ft_found.c
+++ src/builtin/ft_found.c
@@ -6,11 +6,13 @@
 /*   By: apantiez <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/05/23 17:04:15 by apantiez          #+#    #+#             */
-/*   Updated: 2014/05/23 17:04:15 by apantiez         ###   ########.fr       */
+/*   Updated: 2014/05/26 16:59:43 by mschmit          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <libft.h>
 #include "builtin.h"
+#include "exec.h"
 
 int		ft_strstrlen(char **s)
 {
@@ -62,10 +64,16 @@ int		verif_cd(t_gen *gen)
 {
 	int	i;
 
-	gen = ft_setenv("HOME", "/", 0, gen);
-	gen = ft_setenv("PWD", "/", 0, gen);
-	gen = ft_setenv("OLDPWD", "/", 0, gen);
-	if (ft_strcmp(gen->env[ft_found("PWD", gen)] + 4, "/"))
+	i = 0;
+	if (!get_var_env("HOME", gen->env))
+		gen = ft_setenv("HOME", "/HOME", 0, gen);
+	if (!get_var_env("PWD", gen->env))
+	{
+		gen = ft_setenv("PWD", "/", 0, gen);
+		(ft_strcmp(gen->env[ft_found("PWD", gen)] + 4, "/"));
 		i = chdir("/");
+	}
+	if (!get_var_env("OLDPWD", gen->env))
+		gen = ft_setenv("OLDPWD", "/", 0, gen);
 	return (i);
 }
